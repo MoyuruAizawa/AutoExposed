5946de8df889fc1f76a134fd585d5e70551c81d0
MoyuruAizawa <lvla0805@gmail.com>
2018-11-08 17:15:42 +0900

    support uniqueIndex

--- annotation/src/main/kotlin/io/moyuru/autoexposed/annotation/Column.kt
+++ annotation/src/main/kotlin/io/moyuru/autoexposed/annotation/Column.kt
@@ -2,4 +2,7 @@ package io.moyuru.autoexposed.annotation
 
 @Target(AnnotationTarget.PROPERTY)
 @Retention(AnnotationRetention.SOURCE)
-annotation class Column(val length: Int = 0, val autoIncrement: Boolean = false)
+annotation class Column(
+    val length: Int = 0,
+    val autoIncrement: Boolean = false,
+    val uniqueIndex: Boolean = false)
